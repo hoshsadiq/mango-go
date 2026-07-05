2bc9b54892c87c3d2040e3827e7f4ff6733e1b16
Hosh Sadiq <600045+hoshsadiq@users.noreply.github.com>
2026-07-05 05:33:04 +0400

    feat: add chapter metadata DB migration

---
+++ internal/assets/migrations/000012_add_chapter_metadata.down.sql
@@ -0,0 +1,8 @@
+DROP INDEX IF EXISTS idx_chapter_metadata_tags_metadata;
+DROP INDEX IF EXISTS idx_chapter_metadata_genres_metadata;
+DROP INDEX IF EXISTS idx_chapter_metadata_authors_metadata;
+DROP INDEX IF EXISTS idx_chapter_metadata_chapter;
+DROP TABLE IF EXISTS chapter_metadata_tags;
+DROP TABLE IF EXISTS chapter_metadata_genres;
+DROP TABLE IF EXISTS chapter_metadata_authors;
+DROP TABLE IF EXISTS chapter_metadata;
